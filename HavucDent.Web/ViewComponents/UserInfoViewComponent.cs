535456fbb0364da4f6d1529bc65d5c7286f94fbb
Hüseyin Burak Kocabıyık <turkves@msn.com>
2024-11-07 14:55:40 +0300

    8 : Personel Yönetimi HTML Güncellemeleri

---
+++ HavucDent.Web/ViewComponents/UserInfoViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using HavucDent.Infrastructure.Identity;
+
+public class UserInfoViewComponent : ViewComponent
+{
+	private readonly UserManager<AppUser> _userManager;
+
+	public UserInfoViewComponent(UserManager<AppUser> userManager)
+	{
+		_userManager = userManager;
+	}
+
+	public async Task<IViewComponentResult> InvokeAsync()
+	{
+		var userName = "Misafir";
+
+		if (User.Identity.IsAuthenticated)
+		{
+			var currentUser = await _userManager.GetUserAsync((System.Security.Claims.ClaimsPrincipal)User);
+			if (currentUser != null)
+			{
+				userName = $"{currentUser.FirstName} {currentUser.LastName}";
+			}
+		}
+
+		return View("Default", userName);
+	}
+}
